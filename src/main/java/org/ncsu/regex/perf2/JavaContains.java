4f5e48192fd5d025ff1fd6fc0b13686f1d02e019
peipei <wangpeipei.90@gmail.com>
2020-05-15 09:58:28 -0400

    add not Compile

--- src/main/java/org/ncsu/regex/perf2/JavaContains.java
+++ src/main/java/org/ncsu/regex/perf2/JavaContains.java
@@ -1,17 +1,23 @@
 package org.ncsu.regex.perf2;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.results.BenchmarkResult;
+import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -23,8 +29,8 @@ import org.openjdk.jmh.runner.options.TimeValue;
 @State(Scope.Thread)
 
 @Fork(value = 1, jvmArgs = { "-server", "-Xms2G", "-Xmx2G" }) // heap size
-@Warmup(iterations = 10,time=1)
-@Measurement(iterations = 50,time=1)
+@Warmup(iterations = 10)
+@Measurement(iterations = 50)
 
 
 public class JavaContains {
@@ -40,10 +46,16 @@ public class JavaContains {
 	@Param("true")
 	private boolean expectation;
 	
+	private Pattern p;
+	@Setup(Level.Trial)
+	public void check(){
+		p=Pattern.compile(regex);
+	}
+	
 //	boolean result;
 	
 	@Benchmark
-    public boolean wellHelloThere() {
+    public boolean baselineOverhead() {
 		return expectation;
         // this method was intentionally left blank to measure the infrastructure overheads
 		//assert result==expectation: "Wrong String Ops of Regex matching";
@@ -52,6 +64,13 @@ public class JavaContains {
 	@Benchmark
 	public boolean notCompiledRegexFullMatching(){
 		return Pattern.matches(regex,testString);
+		//result=Pattern.matches(regex,testString);
+		//assert result==expectation: "Wrong String Ops of Regex matching";
+	}
+	
+	@Benchmark
+	public boolean compiledRegexFullMatching(){
+		return p.matcher(testString).matches();
 		//result=Pattern.matches(regex,testString);
 		//assert result==expectation: "Wrong String Ops of Regex matching";
 	}
@@ -67,7 +86,7 @@ public class JavaContains {
 		/**
 		 * commands:
 		 * java -jar target/regexbenchmarks.jar org.ncsu.regex.perf2.JavaContains
-		 * -f 1 -gc true -wi 10 -i 50 -r 100ms -p regex=".*error.*" -p str="error" -p testString="abcccerrordefg" -p expectation="true"
+		 * -f 1 -gc true -wi 10 -w 100ms -i 50 -r 100ms -p regex=".*error.*" -p str="error" -p testString="abcccerrordefg" -p expectation="true"
 		 * -rf csv -rff contains_error_iter50.csv -o log/contains_error_iter50.log
 		 */
 		Options opt = new OptionsBuilder()
@@ -77,6 +96,12 @@ public class JavaContains {
 				.measurementTime(TimeValue.milliseconds(10))
 				.build();
 
-		new Runner(opt).run();
+//		new Runner(opt).run();
+		Collection<RunResult> res=new Runner(opt).run();
+		for(RunResult run_res:res) {
+			BenchmarkResult bench_res=run_res.getAggregatedResult();
+//			Collection<BenchmarkResult> data=run_res.getBenchmarkResults();
+			System.out.println(bench_res.getPrimaryResult());
+		}
 	}
 }
