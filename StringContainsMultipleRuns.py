a4292a3910fd29382cb2af4597581190bde21ae9
PW <pw@Patricks-MacBook-Pro.local>
2021-03-24 16:37:46 -0400

    restricted regex run

--- StringContainsMultipleRuns.py
+++ StringContainsMultipleRuns.py
@@ -35,28 +35,28 @@ if __name__ == '__main__':
     runs = 10
     lens = [9, 17, 63, 125, 318, 948]
         
-    file_name = "http_test_10runs.input4"
-    file_name2 = "http_test_10runs.input5"
-    strings, strings2 = [], []
-    for str_len in lens:
-        gen_str = generate_match_str(".*" + re.escape(SUBSTR_LITERAL) + ".*", str_len)
-        
-        match_pos = gen_str.index(SUBSTR_LITERAL)
-        
-        gen_str2 = generate_random_nonmatching_str(match_pos, substr_regex, character_type) + \
-        SUBSTR_LITERAL + generate_random_nonmatching_str(len(gen_str) - match_pos - len(SUBSTR_LITERAL), substr_regex, character_type) 
-        
-        strings.append((gen_str, str_len, True, len(gen_str)))
-        strings2.append((gen_str2, str_len, True, len(gen_str2)))
-        
-        gen_str_nonmatching = generate_random_nonmatching_str(len(gen_str), substr_regex, character_type)
-        gen_str_nonmatching2 = generate_random_nonmatching_str(len(gen_str), substr_regex, character_type)
-        
-        strings.append((gen_str_nonmatching, str_len, False, len(gen_str_nonmatching)))
-        strings2.append((gen_str_nonmatching2, str_len, False, len(gen_str_nonmatching2)))
-        
-    pickle.dump(strings, open(file_name,"wb"))
-    pickle.dump(strings2, open(file_name2,"wb"))
+#     file_name = "http_test_10runs.input4"
+#     file_name2 = "http_test_10runs.input5"
+#     strings, strings2 = [], []
+#     for str_len in lens:
+#         gen_str = generate_match_str(".*" + re.escape(SUBSTR_LITERAL) + ".*", str_len)
+#         
+#         match_pos = gen_str.index(SUBSTR_LITERAL)
+#         
+#         gen_str2 = generate_random_nonmatching_str(match_pos, substr_regex, character_type) + \
+#         SUBSTR_LITERAL + generate_random_nonmatching_str(len(gen_str) - match_pos - len(SUBSTR_LITERAL), substr_regex, character_type) 
+#         
+#         strings.append((gen_str, str_len, True, len(gen_str)))
+#         strings2.append((gen_str2, str_len, True, len(gen_str2)))
+#         
+#         gen_str_nonmatching = generate_random_nonmatching_str(len(gen_str), substr_regex, character_type)
+#         gen_str_nonmatching2 = generate_random_nonmatching_str(len(gen_str), substr_regex, character_type)
+#         
+#         strings.append((gen_str_nonmatching, str_len, False, len(gen_str_nonmatching)))
+#         strings2.append((gen_str_nonmatching2, str_len, False, len(gen_str_nonmatching2)))
+#         
+#     pickle.dump(strings, open(file_name,"wb"))
+#     pickle.dump(strings2, open(file_name2,"wb"))
     
     JAVA_CLASS_NAME = "benchmark.StringContains"
     cmds = []
@@ -64,11 +64,12 @@ if __name__ == '__main__':
     for idx_dataset, file_name in enumerate(["http_test_10runs.input4", "http_test_10runs.input5"]):
         data = pickle.load(open(file_name, "rb"))
         for gen_str, str_len, matches, true_len in data:
-            print("length:", str_len, "generated str:", gen_str, "matches:", matches, "string length:", true_len)
-            for idx_run in range(runs):
-                cmd = get_cmd(class_path, JAVA_CLASS_NAME, '_'.join(["dataset" + str(idx_dataset + 4), \
-                    str(str_len), str(matches), str(idx_run), "run"]), re.escape(SUBSTR_LITERAL), gen_str)
-                cmds.append(cmd)
+            if (matches, true_len) in [(False, 90)]: #, (True, 137), (False, 137), (False, 318), (True, 1537), (False, 1537)]:
+                print("length:", str_len, "generated str:", gen_str, "matches:", matches, "string length:", true_len)
+                for idx_run in range(runs):
+                    cmd = get_cmd(class_path, JAVA_CLASS_NAME, '_'.join(["dataset" + str(idx_dataset + 4), \
+                        str(str_len), str(matches), str(idx_run), "run"]), re.escape(SUBSTR_LITERAL), gen_str)
+                    cmds.append(cmd)
     
     random.shuffle(cmds)
     for cmd in cmds:
